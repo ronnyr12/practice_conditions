f1335ac37e5844f6c4bc2914e9211a0a89e60787
Ronny <ronny761@hityash.org>
2021-11-02 10:32:57 +0200

    checking uploading to server and to all the braches

--- src/com/company/SADNA12.java
+++ src/com/company/SADNA12.java
@@ -4,5 +4,6 @@ public class SADNA12 {
     public static void main(String[] args) {
         //hw of today - finish the whole exercises
         System.out.println("class cs 2021 rules!");
+        System.out.println("ok");
     }
 }
