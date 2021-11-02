28fc2b3b2c552673669e80c0e5faf611c7c558a4
Ronny <ronny761@hityash.org>
2021-11-02 10:21:00 +0200

    checking uploading to server and to all the braches

--- src/com/company/SADNA12.java
+++ src/com/company/SADNA12.java
@@ -3,5 +3,6 @@ package com.company;
 public class SADNA12 {
     public static void main(String[] args) {
         //hw of today - finish the whole exercises
+        System.out.println("king artem");
     }
 }
