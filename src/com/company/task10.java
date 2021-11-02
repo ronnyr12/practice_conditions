ac6248ecea98902ca5faee0c7cac749e187756e5
donutnotnut <artem75058@gmail.com>
2021-11-02 14:44:44 +0200

    update homework p3(task10 completed)

--- src/com/company/task10.java
+++ src/com/company/task10.java
@@ -1,4 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class task10 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a,b,c;
+        System.out.println("enter 3 sides");
+        a = input.nextInt();
+        b = input.nextInt();
+        c = input.nextInt();
+        if (a+b>c || a+c>b || b+c>a)
+            System.out.println("you cant!!!!");
+        else
+            System.out.println("you can");
+    }
 }
